a8da1fc9221ecfe16a22481f4d04e127c106cd25
ydong <1094035520@qq.com>
2018-12-14 09:21:44 +0800

    dev: Todo 后面将本地的离线人脸识别使用策略模式改为：红软、百度、opencv、兼顾离线识别，方便切换  (tzutalin / dlib-android-app还待验证，项目久了，发现运行不了了)

--- app/src/main/java/com/face/FaceMatchActivity.java
+++ app/src/main/java/com/face/FaceMatchActivity.java
@@ -1,10 +1,16 @@
 package com.face;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -28,6 +34,7 @@ public class FaceMatchActivity extends Activity implements IFaceDetectListner {
         setContentView(R.layout.activity_face_match);
         bind = ButterKnife.bind(this);
         face_camera.setFaceDetectListner(this);
+        initPermission();
     }
 
     @OnClick({R.id.face_match})
@@ -66,4 +73,45 @@ public class FaceMatchActivity extends Activity implements IFaceDetectListner {
         super.onDestroy();
         bind.unbind();
     }
+
+
+    /**
+     * android 6.0 以上需要动态申请权限
+     */
+    private void initPermission() {
+        String[] permissions = {
+                Manifest.permission.INTERNET,
+                Manifest.permission.ACCESS_NETWORK_STATE,
+                Manifest.permission.MODIFY_AUDIO_SETTINGS,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_SETTINGS,
+                Manifest.permission.READ_PHONE_STATE,
+                Manifest.permission.ACCESS_WIFI_STATE,
+                Manifest.permission.CHANGE_WIFI_STATE,
+                Manifest.permission.CAMERA,
+                Manifest.permission.RECORD_AUDIO
+
+        };
+
+        ArrayList<String> toApplyList = new ArrayList<String>();
+
+        for (String perm : permissions) {
+            if (PackageManager.PERMISSION_GRANTED != ContextCompat.checkSelfPermission(this, perm)) {
+                toApplyList.add(perm);
+                // 进入到这里代表没有权限.
+            }
+        }
+        String[] tmpList = new String[toApplyList.size()];
+        if (!toApplyList.isEmpty()) {
+            ActivityCompat.requestPermissions(this, toApplyList.toArray(tmpList), 123);
+        }
+
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        // 此处为android 6.0以上动态授权的回调，用户自行实现。
+    }
+
 }
